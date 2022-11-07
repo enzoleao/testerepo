0784a1efba1dfd6ab3c7593304f94bf3b5de3208
unknown <enzopinheiro6@gmail.com>
2022-11-07 13:43:33 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import TextField from '@mui/material/TextField';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-
+import 'dayjs/locale/pt-br';
+const locales = ['pt-br']
 export default function App() {
+  const [locale, setLocale] = React.useState('pt-br');
   const [value, setValue] = React.useState(null);
   const isWeekend = (date) => {
     const day = date.day();
@@ -13,9 +15,11 @@ export default function App() {
     return day === 0 || day === 6;
   };
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <LocalizationProvider dateAdapter={AdapterDayjs}
+    adapterLocale={locale}
+    >
     <DatePicker
-      disabledPast
+      disablePast
       shouldDisableDate={isWeekend}
       label="Basic example"
       value={value}
