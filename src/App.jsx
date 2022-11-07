7e7180dcebcf0c82ab0d496429a6148af294e770
unknown <enzopinheiro6@gmail.com>
2022-11-07 15:41:21 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,20 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import 'dayjs/locale/pt-br';
+import { set } from 'date-fns';
 const locales = ['pt-br']
 export default function App() {
   const [locale, setLocale] = React.useState('pt-br');
   const [value, setValue] = React.useState(null);
+  const [dataNew, setDataNew] = React.useState(null)
   const isWeekend = (date) => {
     const day = date.day();
   
     return day === 0 || day === 6;
+
   };
+  
+  
   return (
     <>
     <LocalizationProvider dateAdapter={AdapterDayjs}
@@ -24,12 +29,14 @@ export default function App() {
       shouldDisableDate={isWeekend}
       label="Basic example"
       inputFormat="DD-MM-YYYY"
-      value={value}
+      value={value}     
       onChange={(newValue) => {
-        const test = newValue.format("MM-DD-YYYY")
-        setValue(test);
+        setValue(newValue.format("MM-DD-YYYY")); 
       }}
-      renderInput={(params) => <TextField {...params} />}
+      renderInput={(params) => <TextField
+      
+        {...params} 
+      />}
     />
   </LocalizationProvider>
       <button onClick={console.log(value)}>teste</button>
